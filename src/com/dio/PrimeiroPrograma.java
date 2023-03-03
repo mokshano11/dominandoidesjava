0521d2157d05f02c5c45cfc13f3ec319514d16d1
kali <kali@kali>
2023-03-03 15:18:09 -0500

    primeiros passos

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+package com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+
+		gato gato = new gato();
+		livros livros = new livros();
+		System.out.println(gato);
+		System.out.println(livros);
+
+		/*
+		 * int a = 2; int b = 3; System.out.println("hello world " + (a+b));
+		 */
+	}
+}
+	class livros {
+		private String nome;
+		private String npag;
+	}
+
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kali
+ *
+ */
+module primeiroprograma {
+}
